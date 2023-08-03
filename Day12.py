e59eaa73a396ddefa5724408c292c0d77d3dfddd
aaditjainofficial360 <105768739+aaditjainofficial360@users.noreply.github.com>
2023-08-03 23:21:17 +0530

    Pending

---
+++ Day12.py
@@ -0,0 +1,17 @@
+'''
+********DAY 12********
+Python coding question:
+
+Write a code to check whether a year is leap year or not.
+Note:A year that is evenly divisible by 100 is a leap year only if it is also evenly divisible by 400.
+
+Desired Output:
+is_leap(1996)-->Leap year
+is_leap(2100)-->not a Leap year
+is_leap(2000)—>Leap year
+'''
+
+def is_leap(year):
+    pass
+
+print(is_leap(1996))
